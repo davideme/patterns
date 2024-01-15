37e4d83f3b41ac7f0bb03f78700bdd5c33bb228a
Davide Mendolia <davide@gokarumi.com>
2024-01-16 00:32:34 +0100

    functional version

--- typescript/src/patterns/Behavioral/Blackboard.ts
+++ typescript/src/patterns/Behavioral/Blackboard.ts
@@ -1,4 +1,3 @@
-// Blackboard
 class Blackboard {
   private data: Record<string, any> = {}
 
@@ -11,33 +10,27 @@ class Blackboard {
   }
 }
 
-// Knowledge Source Interface
-interface KnowledgeSource {
-  update: (blackboard: Blackboard) => void
+// Knowledge Source as a higher-order function type
+type KnowledgeSource = (blackboard: Blackboard) => void
+
+// Concrete Knowledge Sources as functions
+const expert1: KnowledgeSource = (blackboard: Blackboard): void => {
+  console.log('Expert1 updating blackboard')
+  blackboard.setData('expert1Data', 'dataFromExpert1')
 }
 
-// Experts implementing the KnowledgeSource interface
-class Expert1 {
-  update (blackboard: Blackboard): void {
-    console.log('Expert1 updating blackboard')
-    blackboard.setData('expert1Data', 'dataFromExpert1')
-  }
-}
-
-class Expert2 {
-  update (blackboard: Blackboard): void {
-    console.log('Expert2 updating blackboard')
-    blackboard.setData('expert2Data', 'dataFromExpert2')
-  }
+const expert2: KnowledgeSource = (blackboard: Blackboard): void => {
+  console.log('Expert2 updating blackboard')
+  blackboard.setData('expert2Data', 'dataFromExpert2')
 }
 
 // Control
 class Controller {
   private readonly blackboard = new Blackboard()
-  private readonly experts: KnowledgeSource[] = [new Expert1(), new Expert2()]
+  private readonly experts: KnowledgeSource[] = [expert1, expert2]
 
   execute (): void {
-    this.experts.forEach(expert => expert.update(this.blackboard))
+    this.experts.forEach(expert => expert(this.blackboard))
     console.log('Final Blackboard Data:', this.blackboard)
   }
 }
