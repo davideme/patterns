db542852493ee6a163e1c91d6381148b3cd32587
Davide Mendolia <davide@gokarumi.com>
2024-01-15 23:04:26 +0100

    add vitest

---
+++ typescript/vite.config.js
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'Patterns'
+    }
+  }
+})
