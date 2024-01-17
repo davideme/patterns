2017b55af89a6544df3acf72b2a3be7496dcde21
Davide Mendolia <davide@gokarumi.com>
2024-01-17 13:57:25 +0100

    linter

--- typescript/src/patterns/Behavioral/Iterator.ts
+++ typescript/src/patterns/Behavioral/Iterator.ts
@@ -1,13 +1,13 @@
-function* createArrayIterator<T>(collection: T[]): Generator<T, void, undefined> {
-    for (let item of collection) {
-        yield item;
-    }
+function * createArrayIterator<T> (collection: T[]): Generator<T, void, undefined> {
+  for (const item of collection) {
+    yield item
+  }
 }
 
 // Usage
 const numbers = [1, 2, 3, 4, 5]
 const iterator = createArrayIterator(numbers)
 
-for (let value of iterator) {
-    console.log(value);
+for (const value of iterator) {
+  console.log(value)
 }
