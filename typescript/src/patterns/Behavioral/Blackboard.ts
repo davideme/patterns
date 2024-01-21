49aab18340231040abbb2905616357b4831ec5e0
Davide Mendolia <davide@gokarumi.com>
2024-01-21 10:18:29 +0100

    improve Blackboard

--- typescript/src/patterns/Behavioral/Blackboard.ts
+++ typescript/src/patterns/Behavioral/Blackboard.ts
@@ -1,33 +1,43 @@
-class Blackboard {
+interface IBlackboard {
+  setData: (key: string, value: any) => void
+  getData: (key: string) => any
+}
+
+class Blackboard implements IBlackboard {
   private data: Record<string, any> = {}
 
   setData (key: string, value: any): void {
     this.data[key] = value
   }
 
-  getData (key: string): Record<string, any> {
+  getData (key: string): any {
     return this.data[key]
   }
 }
 
 // Knowledge Source as a higher-order function type
-type KnowledgeSource = (blackboard: Blackboard) => void
+type KnowledgeSource = (blackboard: IBlackboard) => void
 
 // Concrete Knowledge Sources as functions
-const expert1: KnowledgeSource = (blackboard: Blackboard): void => {
+const expert1: KnowledgeSource = (blackboard: IBlackboard): void => {
   console.log('Expert1 updating blackboard')
   blackboard.setData('expert1Data', 'dataFromExpert1')
 }
 
-const expert2: KnowledgeSource = (blackboard: Blackboard): void => {
+const expert2: KnowledgeSource = (blackboard: IBlackboard): void => {
   console.log('Expert2 updating blackboard')
   blackboard.setData('expert2Data', 'dataFromExpert2')
 }
 
 // Control
 class Controller {
-  private readonly blackboard = new Blackboard()
-  private readonly experts: KnowledgeSource[] = [expert1, expert2]
+  private readonly blackboard: IBlackboard
+  private readonly experts: KnowledgeSource[]
+
+  constructor (blackboard: IBlackboard, experts: KnowledgeSource[]) {
+    this.blackboard = blackboard
+    this.experts = experts
+  }
 
   execute (): void {
     this.experts.forEach(expert => expert(this.blackboard))
@@ -36,5 +46,5 @@ class Controller {
 }
 
 // Example usage
-const controller = new Controller()
+const controller = new Controller(new Blackboard(), [expert1, expert2])
 controller.execute()
